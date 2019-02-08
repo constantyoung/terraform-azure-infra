26c115e8a9f9d1fe65175f34ec478d2065b449ab
Constant Young <constant_young@onecallcm.com>
2019-02-08 15:20:14 -0500

    added more resources and configuration to vm resource

--- main.tf
+++ main.tf
@@ -11,12 +11,14 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = "${azurerm_resource_group.rg.name}"
   address_space       = ["10.0.0.0/16"]
 
-  subnet {
-    name           = "subnet1"
-    address_prefix = "10.0.1.0/24"
-  }
-
   tags = "${var.tags}"
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet1"
+  resource_group_name  = "${azurerm_resource_group.rg.name}"
+  virtual_network_name = "${azurerm_virtual_network.vnet.name}"
+  address_prefix       = "10.0.1.0/24"
 }
 
 resource "azurerm_lb" "lb" {
@@ -32,6 +34,15 @@ resource "azurerm_lb" "lb" {
   tags = "${var.tags}"
 }
 
+resource "azurerm_public_ip" "lbpip" {
+  name                = "loadbalancerpip"
+  location            = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  allocation_method   = "Static"
+
+  tags = "${var.tags}"
+}
+
 resource "azurerm_availability_set" "as" {
   name                = "availabilityset1"
   location            = "${azurerm_resource_group.rg.location}"
@@ -41,11 +52,13 @@ resource "azurerm_availability_set" "as" {
 }
 
 resource "azurerm_virtual_machine" "vm" {
+  count                 = 2
   name                  = "vmname${count.index}"
   location              = "${azurerm_resource_group.rg.location}"
   resource_group_name   = "${azurerm_resource_group.rg.name}"
-  network_interface_ids = ["${azurerm_network_interface.nic.id}"]
+  network_interface_ids = ["${azurerm_network_interface.nic.*.id}"]
   vm_size               = "Standard D2s v3"
+  availability_set_id   = "${azurerm_availability_set.as.id}"
 
   # Uncomment this line to delete the OS disk automatically when deleting the VM
   # delete_os_disk_on_termination = true
@@ -74,5 +87,32 @@ resource "azurerm_virtual_machine" "vm" {
   os_profile_linux_config {
     disable_password_authentication = false
   }
+  tags = "${var.tags}"
+}
+
+resource "azurerm_network_interface" "nic" {
+  count               = 2
+  name                = "vmname-${count.index}-nic"
+  location            = "${azurerm_resource_group.rg.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = "${azurerm_subnet.subnet.id}"
+    private_ip_address_allocation = "Dynamic"
+  }
+  ip_configuration {
+      name = "pipconfiguration"
+      public_ip_address_id = "${azurerm_public_ip.vmpips.id[count.index]}"
+  }
+}
+
+resource "azurerm_public_ip" "vmpips" {
+  count               = 2
+  name                = "vmpip${count.index}"
+  location            = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  allocation_method   = "Static"
+
   tags = "${var.tags}"
 }
